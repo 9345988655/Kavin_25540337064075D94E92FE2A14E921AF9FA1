79bbb895e34d2670f91cc5f900f04f20c2a84710
9345988655 <144461527+9345988655@users.noreply.github.com>
2023-10-09 11:42:57 +0530

    Add files via upload

---
+++ program 6.py
@@ -0,0 +1,22 @@
+class student:
+
+   def __init__(self, name, roll_number, cgpa):
+     self.name = name 
+     self.roll_number = roll_number 
+     self.cgpa = cgpa 
+
+def sort_students(student_list):
+  #sort  the  list of  student in descending order of cgpa 
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,reverse=True)
+  # syntax - lambda arg:exp 
+  return sorted_students 
+
+# Example usage:
+students = [
+   student("Hari", "A123", 7.8),
+   student("srikanth", "A124", 8.9),
+   student("saumya", "A125", 9.1),
+   student("mahidhar", "A126", 9.9),
+  ]
+sorted_students = sort_students(students)
